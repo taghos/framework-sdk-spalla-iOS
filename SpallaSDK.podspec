3738d1905fc769b945cc2c6195ad0c58d463b6ae
Rogerio Shimizu <roja@bunker79.com>
2024-10-07 18:53:00 -0300

    Release v0.7.2

--- SpallaSDK.podspec
+++ SpallaSDK.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
     s.name          = 'SpallaSDK'
-    s.version       = '0.7.1'
+    s.version       = '0.7.2'
     s.summary       = 'Spalla Player with Analytics'
     s.homepage      = 'https://github.com/taghos/framework-sdk-spalla-iOS'
     s.license       = { :type => 'MIT' }
--- SpallaSDK.xcframework/ios-arm64_x86_64-simulator/SpallaSDK.framework/build.sh
+++ SpallaSDK.xcframework/ios-arm64_x86_64-simulator/SpallaSDK.framework/build.sh
@@ -6,22 +6,7 @@ xcodebuild archive -workspace SpallaWorkspace.xcworkspace -scheme SpallaSDK -sdk
 xcodebuild archive -workspace SpallaWorkspace.xcworkspace -scheme SpallaSDK -sdk iphonesimulator -archivePath archives/SpallaFramework-iOS_SimulatorArm -arch arm64
 
 
-#create fat framework
-#lipo -create \
-./archives/SpallaFramework-iOS.xcarchive/Products/Library/Frameworks/SpallaSDK.framework/SpallaSDK \
-  ./archives/SpallaFramework-iOS_Simulator.xcarchive/Products/Library/Frameworks/SpallaSDK.framework/SpallaSDK \
-  -output ./archives/SpallaFramework-Simulator
-
-#copy generated framework over the simulator one (kinda hacky)
-#cp ./archives/SpallaFramework-Simulator ./archives/SpallaFramework-iOS_Simulator.xcarchive/Products/Library/Frameworks/SpallaSDK.framework/SpallaSDK
-
-#xcodebuild -create-xcframework \
-  -framework ./archives/SpallaFramework-iOS_Simulator.xcarchive/Products/Library/Frameworks/SpallaSDK.framework \
-  -framework ./archives/SpallaFramework-iOS.xcarchive/Products/Library/Frameworks/SpallaSDK.framework \
-  -output ./Products/SpallaFramework.xcframework
-
 xcodebuild -create-xcframework \
 -archive archives/SpallaFramework-iOS.xcarchive -framework SpallaSDK.framework \
 -archive archives/SpallaFramework-iOS_Simulator.xcarchive -framework SpallaSDK.framework \
--archive archives/SpallaFramework-iOS_SimulatorArm.xcarchive -framework SpallaSDK.framework \
 -output xcframeworks/SpallaSDK.xcframework
